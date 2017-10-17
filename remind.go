be98c75214ae6ce333f388253d14e996394acb6f
Riley Johnson <rileyjohnson99@hotmail.com>
2017-10-16 22:01:41 -0500

    Adding proper documentation, will polish up the rest soon

--- remind.go
+++ remind.go
@@ -59,6 +59,7 @@ type fullLyricMessage struct {
 	Body fullLyricBody `json:"message"`
 }
 
+//Gets the song ID for the corresponding track and artist (if included)
 func getSongID(artist, track, api string) string {
 	base := "http://api.musixmatch.com/ws/1.1/track.search?apikey=" + api
 	var b bytes.Buffer
@@ -104,47 +105,67 @@ func getSongLyrics(ID, api string) string {
 }
 
 func main() {
-	var artist, title, keys, span, from, to string
+	var artist, title, keys, span, from, to string //Variables to hold the command line args corresponding to
 	flag.StringVar(&artist, "artist", "", "The name of the artist of the song you want to lookup")
 	flag.StringVar(&title, "title", "", "The name of the song you want to lookup")
 	flag.StringVar(&to, "to", "", "The phone number you're sending to in the format '+(Country Code)(Area Code)(Phone Number)', ex. '+17015559999")
 	flag.StringVar(&from, "from", "", "The twilio number you're sending from, if not included, it's assumed that you have it in your keys .json file")
-	flag.StringVar(&keys, "keys", "", "The location of the keys for the Twilio and MusixMatch API's")
+	flag.StringVar(&keys, "keys", "", "The location of the keys for the Twilio and MusixMatch API's, json should look like: ")
 	flag.StringVar(&span, "span", "", "The time span over which the lyrics are to be sent every 'span' / 'number of verses' amount of time")
 	flag.Parse()
+
+	//Making sure the nessesary parameters are included
 	if keys == "" {
 		log.Fatal("Need the location for the API keys, please")
+	}
+	if title == "" {
+		log.Fatal("Need at least a title for the song")
 	}
 	if to == "" {
 		log.Fatal("Need a phone number to send to")
 	}
+
+	//Reading the data in from the location specified by the "keys" argument
 	var dat datAuth
 	bdata, err := ioutil.ReadFile(keys)
 	if err != nil {
 		log.Fatal("Could not read data properly")
 	}
-	err = json.Unmarshal(bdata, &dat)
-	if err != nil {
+
+	//Unmarshaling the data into a datAuth struct to hold sensative information
+	if json.Unmarshal(bdata, &dat) != nil {
 		log.Fatal("Error Unmarshalling the data")
 	}
-	if from == "" {
+
+	//Making sure that there is a sending Twilio number
+	if from == "" && dat.Number != "" {
 		from = dat.Number
+	} else if from == "" && dat.Number == "" {
+		log.Fatal("Need a sending Twilio number")
 	}
+
+	//Get the ID, and then get the lyrics with the returning ID from that function
 	lyrics := getSongLyrics(getSongID(artist, title, dat.Lkey), dat.Lkey)
 	client := twilio.NewClient(dat.Sid, dat.Token, nil)
 
-	//Assuring the starting date argument isn't empty and if it is defaulting to right now
+	//Split the lyrics up by the sections of lyrics seperated by two newlines
+	slyrics := strings.Split(lyrics, "\n\n")
+
+	//Assuring the starting date argument isn't empty and if it is defaulting to sending the lyrics right now
 	if span != "" {
-		tTime, err := time.ParseDuration(span) //Parse the start time into the local time
+		tTime, err := time.ParseDuration(span) //Parse the sending interval and check for success
 		if err != nil {
-			log.Fatal("Error parsing starting time")
+			log.Fatal("Error parsing sending interval")
 		}
-		slyrics := strings.Split(lyrics, "\n\n")
+
+		//Range over all values except the last which isn't lyrical material,
+		//Then between iterations sleep for the set interval
 		for _, l := range slyrics[:len(slyrics)-1] {
 			time.Sleep(time.Duration(tTime.Nanoseconds() / int64(len(slyrics)-1)))
 			_, err = client.Messages.SendMessage(dat.Number, to, l, nil)
 		}
 	} else {
-		_, err = client.Messages.SendMessage(dat.Number, to, lyrics, nil)
+		//Joining together the split up lyrics, but making sure not to include the final value because it ins't lyrical material
+		_, err = client.Messages.SendMessage(dat.Number, to, strings.Join(slyrics[:len(slyrics)-1], "\n\n"), nil)
 	}
 }
